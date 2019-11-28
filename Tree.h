f15be252d74a20c1b556e4b3c9f67854bc1fd971
Stek40 <39171474+Stek40@users.noreply.github.com>
2019-11-28 20:45:07 +0200

    Update Tree.h

--- Tree.h
+++ Tree.h
@@ -1,12 +1,12 @@
 #pragma once
-#include "node.h"
+#include "Node.h"
 #include <string>
 #include<fstream>
 
 class Tree
 {
 private:
-	node* root;
+	Node* root;
 	int* number_of_symbols;
 
 public:
@@ -15,7 +15,7 @@ public:
 
 	void archive(std::string);
 
-	node* get_root();
+	Node* get_root();
 
 	int* get_number_of_symbols();
 	void set_number_of_symbols(int);
