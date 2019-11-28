5722de844b5ba8c784bf39a8224842619f50ec87
Stek40 <39171474+Stek40@users.noreply.github.com>
2019-11-28 23:16:17 +0200

    Update Tree.h

--- Tree.h
+++ Tree.h
@@ -2,21 +2,24 @@
 #include "Node.h"
 #include <string>
 #include<fstream>
+using namespace std;
 
 class Tree
 {
 private:
 	Node* root;
-	int* number_of_symbols;
+	
 
 public:
 	Tree();
+	Tree(char, int);
 	~Tree();
 
 	void archive(std::string);
+	vector<Node> zeroless_and_sorted(int*);
 
 	Node* get_root();
 
-	int* get_number_of_symbols();
-	void set_number_of_symbols(int);
+	//int* get_number_of_symbols();
+	//void set_number_of_symbols(int);
 };
