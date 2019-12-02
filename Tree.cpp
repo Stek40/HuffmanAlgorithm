95e5cbd75e124e1d3c63c53645e61c3ae9d2c5cd
Stek40 <39171474+Stek40@users.noreply.github.com>
2019-12-02 10:22:10 +0200

    Update Tree.cpp

--- Tree.cpp
+++ Tree.cpp
@@ -2,9 +2,7 @@
 #include "Tree.h"
 #include <iostream>
 #include <vector> 
-#include <algorithm>
-#include <functional>
-#include <array>
+#include <map> 
 using namespace std;
 
 
@@ -19,10 +17,15 @@ Tree::Tree(char ch, int n)
 	root = new Node(ch, n);
 }
 
+Tree::Tree(Node* node)
+{
+	root = node;
+}
+
 
 Tree::~Tree()
 {
-	//delete[] number_of_symbols;
+	
 
 }
 
@@ -37,9 +40,13 @@ void Tree::archive(std::string file_name)
 	cout << number_of_symbols[(int)'a'] << endl
 		<< number_of_symbols[(int)'c'] << endl
 		<< number_of_symbols[(int)'b'] << endl
-		<< number_of_symbols[(int)' '] << endl;
+		<< number_of_symbols[(int)'\n'] << endl;
 	int* n_of_diff_chars = new int[1];
 	Node* nodes = zeroless_and_sorted(number_of_symbols, n_of_diff_chars);
+	cout << number_of_symbols[(int)'a'] << endl
+		<< number_of_symbols[(int)'c'] << endl
+		<< number_of_symbols[(int)'b'] << endl
+		<< number_of_symbols[(int)'\n'] << endl;
 	cout << *n_of_diff_chars << "!!" << endl;
 	for (int i = 0; i < 4; i++)
 	{
@@ -50,20 +57,83 @@ void Tree::archive(std::string file_name)
 
 	for (int i = 0; i < *n_of_diff_chars; i++)
 	{
-		Tree t((char)i, number_of_symbols[i]);
+		Tree t(&nodes[i]);
 		trees.push_back(t);
 	}
 
 	// uniting trees into one big tree
+	cout << "size of trees before uniting:" << trees.size()<<endl;
+	while (trees.size() > 1)
+	{
+		Tree* min1;
+		Tree* min2;
+		int min2_position,
+			min1_position;
+		if (trees[0].root->sum > trees[1].root->sum)
+		{
+			min1 = &trees[0];
+			min2 = &trees[1];
+			min1_position = 0;
+			min2_position = 1;
+		}
+		else 
+		{
+			min1 = &trees[1];
+			min2 = &trees[0];
+			min1_position = 1;
+			min2_position = 0;
+		}
+		
+		
+		for (int i = 2; i < trees.size(); i++)
+		{
+			if (trees[i].root->sum < min1->root->sum)
+			{
+				min2 = min1;
+				min2_position = min1_position;
+				min1 = &trees[i];
+				min1_position = i;
+			}
+			else
+			{
+				if (trees[i].root->sum < min2->root->sum)
+				{
+					min2 = &trees[i];
+					min2_position = i;
+				}
+			}
+		}
+		min1->combineWith(min2);
+		trees.erase(trees.begin() + min2_position);
+	}
 
+	cout << trees[0].root->sum;
+
+	// obrazuvane na 0 i 1 posledovatelnosti za vs simvol
+
+	map<char, string>* symbols_archive = new map<char, string>();
+	symbols_archive->insert(pair<char, string>('a', "010"));
 	
+
+	int found_all = 0;
 	
+	//while (found_all != *n_of_diff_chars)
+	{
+		trees[0];
+	}
 }
 
-Tree* Tree::combineWith(Tree* tree)
+void Tree::mappingSymbols(map<char, string>* symbols_archive, Node* node)
 {
-	Node* new_root = new Node('null', this->get_root.sum + tree->get_root.sum);
-	// unite 2 trees and return it
+
+}
+
+void Tree::combineWith(Tree* tree)
+{
+	Node* new_root = new Node(this->root->sum + tree->root->sum);
+	new_root->next_left = this->root;
+	new_root->next_right = tree->root;
+	this->root = new_root;
 }
 
 Node* Tree::zeroless_and_sorted(int* arr, int* n_of_diff_chars)
@@ -107,12 +177,3 @@ Node* Tree::get_root()
 {
 	return root;
 }
-/*
-int* Tree::get_number_of_symbols()
-{
-	return number_of_symbols;
-}
-void Tree::set_number_of_symbols(int i)
-{
-	get_number_of_symbols()[i]++;
-}*/
