9ecbc9a0c0096cc06d4fdb3c35dc2f8a7649a7f1
Stek40 <39171474+Stek40@users.noreply.github.com>
2019-12-02 10:24:19 +0200

    Update Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -9,6 +9,12 @@ Node::Node() : sum(0), symbol(0)
 
 }
 
+Node::Node(int sum) : sum(sum)
+{
+	next_left = nullptr;
+	next_right = nullptr;
+}
+
 Node::Node(char symbol, int sum) : sum(sum), symbol(symbol)
 {
 	next_left = nullptr;
@@ -16,7 +22,7 @@ Node::Node(char symbol, int sum) : sum(sum), symbol(symbol)
 }
 
 
-Node& Node::operator= (const Node& other) 
+Node& Node::operator= (const Node& other)
 {
 	if (this != &other) {
 		this->sum = other.sum;
