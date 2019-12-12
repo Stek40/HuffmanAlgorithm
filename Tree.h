49c9ac553ef1ee60a663c39d7461aeba0b173533
Stek40 <39171474+Stek40@users.noreply.github.com>
2019-12-12 09:29:11 +0200

    Update Tree.h

--- Tree.h
+++ Tree.h
@@ -2,12 +2,6 @@
 #include "Node.h"
 #include <string>
 #include<fstream>
-using namespace std;
-
-#pragma once
-#include "Node.h"
-#include <string>
-#include<fstream>
 #include "Pair.h"
 using namespace std;
 
@@ -25,9 +19,13 @@ public:
 	void archive(std::string);
 	Node* zeroless_and_sorted(int*, int*);
 	void combineWith(Tree*);
-	//void mappingSymbols(map<char, string>&, Node*);
-	void mappingSymbols(Pair*);
-	void symbolsTreeSearch(Pair*, Node*, string, int*);
+	void mappingSymbols(Pair**);
+	void symbolsTreeSearch(Pair**, Node*, string, int*);
+	string mapped(Pair**, char, int);
+	string treeToString();
+	string treeToStringRecursion(string, Node*, Node*);
+	string numToStr(int);
 
 	Node* get_root();
+	void set_root(Node*);
 };
